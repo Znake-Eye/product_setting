2b64b1622d4dc8bfd04bc0e9dc3d05e107445f53
manhvichhai <vichhaireal2020@gmail.com>
2023-10-06 00:12:02 +0700

    file migration

---
+++ database/migrations/2023_10_05_163905_supplier.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('supplier', function (Blueprint $table) {
+            $table->id();
+            $table->string('supID')->unique();
+            $table->string('supName');
+            $table->string('supNamekh');
+            $table->string('supEmail');
+            $table->string('supAddress');
+            $table->string('supPerson');
+            $table->string('supPersonphone');
+            $table->string('created_by');
+            $table->string('updated_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('supplier');
+    }
+};
