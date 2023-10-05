7da0e1eb3ed9ac02ef24e47419df57c337fb227a
manhvichhai <vichhaireal2020@gmail.com>
2023-10-05 23:58:09 +0700

    modifies homepage set time

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,8 +1,23 @@
 <template>
     <!-- <Navigation/> -->
     <h1>Home page</h1>
+    <h2>Date : {{ date }}</h2>
+
 </template>
 <script setup>
+    import {ref, reactive, computed, onMounted} from "vue"
+
+    const date = ref('')
+    
+    onMounted(() => {
+        date.value = new Date().toLocaleTimeString()
+        setInterval(()=> {
+                date.value = new Date().toLocaleTimeString()
+            }, 1000)
+    })
+
+   
+    
 
 
 </script>
