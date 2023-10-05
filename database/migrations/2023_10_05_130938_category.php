d00cd0f44adc5b72e0a00afd63edc6ef2b95da8a
manhvichhai <vichhaireal2020@gmail.com>
2023-10-05 23:31:11 +0700

    category migaration

--- database/migrations/2023_10_05_130938_category.php
+++ database/migrations/2023_10_05_130938_category.php
@@ -11,7 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('category', function (Blueprint $table) {
+            $table->id();
+            $table->string('catID');
+            $table->string('catName');
+            $table->string('catNamekh');
+            $table->string('created_by');
+            $table->string('updated_by');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('category');
     }
 };
